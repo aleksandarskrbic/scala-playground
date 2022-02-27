4302c28283a0a61a53e768d99d7e4d61775f4be3
aleksandarskrbic <skrbic.alexa@gmail.com>
2022-02-27 21:59:10 +0100

    Impl fibers

--- modules/zio-from-scratch/src/main/scala/zio/ZIO.scala
+++ modules/zio-from-scratch/src/main/scala/zio/ZIO.scala
@@ -1,7 +1,38 @@
 package zio
 
-sealed trait ZIO[+A] { self =>
+import scala.collection.immutable
+import scala.concurrent.ExecutionContext
 
+trait Fiber[+A] {
+  def start(): Unit
+
+  // 1. Early completion
+  // 2. Late completion
+  def join: ZIO[A]
+}
+
+class FiberImpl[A](zio: ZIO[A]) extends Fiber[A] {
+  var maybeResult: Option[A]    = None
+  var callbacks: List[A => Any] = List.empty[A => Any]
+
+  override def start(): Unit =
+    ExecutionContext.global.execute { () =>
+      zio.run { a =>
+        maybeResult = Some(a)
+        callbacks.foreach(callback => callback(a))
+      }
+    }
+
+  override def join: ZIO[A] =
+    maybeResult match {
+      case Some(a) => ZIO.succeedNow(a)
+      case None    => ZIO.async { complete =>
+        callbacks = complete :: callbacks
+      }
+    }
+}
+
+sealed trait ZIO[+A] { self =>
   // callback or continuation
   def run(callback: A => Unit): Unit
 
@@ -20,11 +51,8 @@ sealed trait ZIO[+A] { self =>
   def flatMap[B](f: A => ZIO[B]): ZIO[B] =
     ZIO.FlatMap(self, f)
 
-  def steps: Int = self match {
-    case ZIO.Effect(_)  => 1
-    case ZIO.Succeed(_) => 1
-    case _              => 0
-  }
+  def fork: ZIO[Fiber[A]] =
+    ZIO.Fork(self)
 }
 
 object ZIO {
@@ -34,6 +62,9 @@ object ZIO {
 
   def succeedNow[A](value: A): ZIO[A] =
     ZIO.Succeed(value)
+
+  def async[A](register: (A => Any) => Any): ZIO[A] =
+    ZIO.Async(register)
 
   case class Succeed[A](value: A) extends ZIO[A] {
     override def run(callback: A => Unit): Unit =
@@ -48,6 +79,19 @@ object ZIO {
   case class FlatMap[A, B](zio: ZIO[A], f: A => ZIO[B]) extends ZIO[B] {
     override def run(callback: B => Unit): Unit =
       zio.run(a => f(a).run(b => callback(b)))
+  }
+
+  case class Async[A](register: (A => Any) => Any) extends ZIO[A] {
+    override def run(callback: A => Unit): Unit =
+      register(callback)
+  }
+
+  case class Fork[A](zio: ZIO[A]) extends ZIO[Fiber[A]] {
+    override def run(callback: Fiber[A] => Unit): Unit = {
+      val fiber = new FiberImpl(zio)
+      fiber.start()
+      callback(fiber)
+    }
   }
 
 }
--- modules/zio-from-scratch/src/main/scala/zio/examples/demos.scala
+++ modules/zio-from-scratch/src/main/scala/zio/examples/demos.scala
@@ -7,8 +7,10 @@ final case class Person(firstname: String, lastname: String)
 sealed trait ZIOApp {
   def run: ZIO[Any]
 
-  def main(args: Array[String]): Unit =
+  def main(args: Array[String]): Unit = {
     run.run(result => println(s"Result was $result"))
+    Thread.sleep(3500)
+  }
 }
 
 object SucceedNow extends ZIOApp {
@@ -24,19 +26,51 @@ object SucceedNowPrint extends ZIOApp {
 object Zip extends ZIOApp {
   val zipped: ZIO[(Int, String)] = ZIO.succeed(1) zip ZIO.succeed("one")
 
-  override def run: ZIO[Any] = ZIO.succeed(zipped.steps)
+  override def run: ZIO[Any] = ZIO.succeed(zipped)
 }
 
 object Map extends ZIOApp {
-  val mapped: ZIO[String] = ZIO.succeed(Person("John", "Doe")).map(_.lastname)
+  val mapped: ZIO[Any] = ZIO.succeed(Person("John", "Doe")).map(_.lastname)
 
-  override def run: ZIO[Any] = mapped
+  override def run = mapped
 }
 
 object FlatMap extends ZIOApp {
-  val mapped: ZIO[String] = ZIO.succeed(Person("John", "Doe")).map(_.lastname)
+  val mapped: ZIO[Any] = ZIO.succeed(Person("John", "Doe")).map(_.lastname)
 
-  val flatMapped = mapped.flatMap(res => ZIO.succeed(println(s"$res")).map(_ => "Done"))
+  val flatMapped: ZIO[Any] = mapped.flatMap(res => ZIO.succeed(println(s"$res")).map(_ => "Done"))
 
   override def run: ZIO[Any] = flatMapped
+}
+
+object Async extends ZIOApp {
+  override def run: ZIO[Any] =
+    ZIO.async[Any] { complete =>
+      println("Async started")
+      Thread.sleep(1000)
+      complete(10)
+    }
+}
+
+object Fork extends ZIOApp {
+
+  val asyncZIO = ZIO.async[Int] { complete =>
+    println("Async started")
+    Thread.sleep(2500)
+    complete(scala.util.Random.nextInt(999))
+  }
+
+  def printLine(message: String): ZIO[Unit] =
+    ZIO.succeed(println(message))
+
+  val forkedZIO =
+    for {
+      fa <- asyncZIO.fork
+      fb <- asyncZIO.fork
+      _ <- printLine("fibers are forked!!!")
+      a <- fa.join
+      b <- fb.join
+    } yield s"Results is $a and $b"
+
+  override def run = forkedZIO
 }
